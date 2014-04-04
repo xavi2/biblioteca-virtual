6f7c9cf9c69a7cb6dc7ee0ac1286a912b0e5e75b
diego-gabriel <diego.gabriel.nd@gmail.com>
2014-04-04 01:38:39 -0400

    Empezamos el Login?
    falta conexion con la base de datos

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<head>
+	
+	<title>Biblioteca Virtual</title>
+
+</head>
+<body>
+	
+	<button> <a href="login.php">Iniciar Sesion </a></button>
+
+</body>
---
+++ login.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<head>
+	
+	<title>Biblioteca Virtual : Iniciar Sesion</title>
+
+</head>
+
+<body>
+	
+	<form action="welcome.php" method: "POST" id = "login">
+		
+		<span>Nombre de usuario o e-mail: </span>
+		<input type = "text" name: "username"> <br>
+		<span>Contrasen&#771;a:</span>
+		<input type = "password" name = "password"> <br>
+		<input type = "submit">
+
+	</form>
+
+</body>
