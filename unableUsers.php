bfc808084b14959add871c20e9c7465a3d8d1d76
diego-gabriel <diego.gabriel.nd@gmail.com>
2014-04-09 15:26:19 -0400

    empezando lista de inabilitados

---
+++ unableUsers.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Usuarios Irregulares : Biblioteca-Virtual</title>
+
+</head>
+<body>
+	<?php
+		global $connection;
+		$connection = mysqli_connect("localhost", "root", "", "biblioteca-virtual");
+		
+		if (mysqli_connect_errno()){
+			echo "<p> Connection Failed </p>";
+		}
+
+		$result = mysqli_query($connection, "SELECT * FROM USUARIO");
+
+		echo "<table border = '1px'
+		<tr>
+			<th>Nombre de usuario</th>
+			<th>Estado:</th>
+			<th></th>
+		</tr>
+		";
+
+		while ($row = mysqli_fetch_array($result)){
+			echo "<tr>";
+
+			echo "<td>". $row['nombre']." ".$row['apellido_Pat']." ".$row['apellido_Mat'];
+
+			echo "</tr>";
+		}
+	?>
+</body>
+</html>
