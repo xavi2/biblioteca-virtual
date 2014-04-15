7b05a062324bf2cd0410e9547b74d84dcd48fee7
diego-gabriel <diego.gabriel.nd@gmail.com>
2014-04-15 04:44:08 -0400

    awsome index falta unir cosas

--- index.php
+++ index.php
@@ -1,22 +1,66 @@
 <!DOCTYPE html>
 
-<?php
-	global $connection;
-	$connection = mysqli_connect("localhost", "root", "", "biblioteca-virtual");
-	
-	if (mysqli_connect_errno()){
-		echo "<p> Connection Failed </p>";
-	}
-?>
-
 <head>
 	
-	<title>Biblioteca Virtual</title>
+	<title> <?php if ($_POST['title']) 
+				echo($_POST['title'] . " - ");
+			?> Biblioteca Virtual	</title>
+	<link href='http://fonts.googleapis.com/css?family=Gochi+Hand' rel='stylesheet' type='text/css'>
 	<link rel = "stylesheet" type="text/css" href = "styles/index.css">
+
+	<script type="text/javascript" src = "jquery/jquery.js"></script>
 </head>
 <body>
-	<div>
-		<h1>Biblioteca Virtual <span>Tu Vieja</span></h1>
-		<button> <a href="login.php">Iniciar Sesion </a></button>
+
+	<script type="text/javascript">
+	 $(function(){
+	 	var status = 0;
+	 	$('#login').hide();
+	 	$('#boton').click(slideLogin);
+
+	 	function slideLogin(){
+	 		status++;
+	 		$('#login').stop().fadeToggle(400, changeArrow);
+	 		
+	 	}
+
+	 	changeArrow();
+
+	 	function changeArrow(){
+	 		console.log(status);
+			var boton = $('#arrow');
+
+	 		if (status % 2 == 0)
+	 			document.getElementById("arrow").innerHTML = ">";
+	 		else
+	 		if (status % 2 == 1)
+	 			document.getElementById("arrow").innerHTML = '<';
+
+	 	}
+	 });
+	</script>
+
+	<h3>Bienvenido!</h3>
+	<div class = "frame">
+		<h1>Biblioteca Virtual: <span id = "tu-vieja">Tu <br> Vieja</span></h1>
+		<div class = "clear"></div>
+		<span id = "boton">Iniciar Sesion  <span id = 'arrow'> > </span> </span>
+
+		
 	</div>
+	<div id = 'login'>
+			<form action="validarUsuario.php" method= "POST">
+				<div>
+				<span>Nombre de usuario: </span>
+				<input type = "text" name = "username"> <br>
+				</div>
+				<div>
+				<span>Contrasen&#771;a:</span>
+				<input type = "text" name = "password"> <br>
+				</div>
+
+				<input type = "submit" class = "centered">
+
+			</form>
+		</div>
 </body>
